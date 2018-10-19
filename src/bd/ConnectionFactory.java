db9915bd517eeefd911d431a73a20757a6f1377a
crmon <crmon93@gmail.com>
2018-10-19 17:16:16 -0300

    Adicionado o metodo listar

--- src/bd/ConnectionFactory.java
+++ src/bd/ConnectionFactory.java
@@ -13,25 +13,24 @@ public class ConnectionFactory {
     //private static final String URL = "jdbc:sqlite:C:\\Users\\chris\\Desktop\\MonitoriaLivraria\\src\\jdbc\\livraria";
     private static final String USUARIO = "";
     private static final String SENHA = "";
-  
     
+    /***
+     * O metodo getConnection retorna um DriverManager.getConnection(URL, USUARIO, SENHA); 
+     */
     public static Connection getConnection(){
-        
-        
-        try {
-            URL = "jdbc:sqlite:"+ new File("src/jdbc/livraria").getCanonicalPath();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-          
-        try{
+             
+        try{  
+            try {
+                System.out.println(new File("src/jdbc/livraria").getCanonicalPath());
+                URL = "jdbc:sqlite:"+ new File("src/jdbc/livraria").getCanonicalPath();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Conectou com sucesso!");   
             return DriverManager.getConnection(URL, USUARIO, SENHA);
         }
         catch(SQLException ex){
             throw new RuntimeException("Erro na conexão!", ex);
-        }
-        finally{
-            System.out.println("Conectou com sucesso!");
         }
     }
 }
--- src/dao/DAOLivro.java
+++ src/dao/DAOLivro.java
@@ -4,6 +4,7 @@ package dao;
 import bd.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,12 +14,13 @@ public class DAOLivro {
     
     public void inserir (Livro livro){
         
-        Connection con = ConnectionFactory.getConnection();  
+        Connection con = ConnectionFactory.getConnection(); 
         final String SQL = "INSERT INTO LIVRO("
         + "NOME, AUTOR, EDITORA, EDICAO, ANOPUBLICACAO)"
         + "VALUES (?, ?, ?, ? , ?)";
         try{ 
             PreparedStatement stmt = con.prepareStatement(SQL);
+            
             stmt.setString(1, livro.getNome());
             stmt.setString(2, livro.getAutor());
             stmt.setString(3, livro.getEditora());
@@ -38,6 +40,32 @@ public class DAOLivro {
                 throw new RuntimeException("Não a conexão não foi fechada! ");
             }
         }
+    }
+    
+    public static void listar(){
+        
+        Connection con = ConnectionFactory.getConnection(); 
+        final String SQL = "SELECT * FROM LIVRO";
+        try{ 
+            PreparedStatement stmt = con.prepareStatement(SQL);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                rs.getString("nome");
+            }
+            //stmt.execute(); 
+        }
+        catch(SQLException e){
+            throw new RuntimeException("Não foi possivel listar na tabela livro\n"+ e);
+        }
+        finally{
+            try {
+                System.out.println("Foi listado com sucesso");
+                con.close();
+            } catch (SQLException ex) {
+                throw new RuntimeException("Não a conexão não foi fechada! ");
+            }
+        }    
     }
     
 }
